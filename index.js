32683ba53689ff91d56975c8cd8d463ac2351a3e
Corey Miller <coreymiller454@gmail.com>
2021-08-11 00:49:33 -0400

    add pagination to query results

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require("cors");
 const Card = require("./models/Card");
 const app = express();
 const axios = require("axios");
@@ -10,118 +10,119 @@ const fs = require("fs");
 const JSONStream = require("JSONStream");
 const https = require("https");
 const { parse } = require("dotenv");
-const cardsRoute = require('./routes/cards');
+const cardsRoute = require("./routes/cards");
+const authRoutes = require("./routes/authRoutes");
 
 require("dotenv").config();
 
 let newData = [];
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}`,
-  { useNewUrlParser: true, useUnifiedTopology: true }
+	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}`,
+	{ useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
-  console.log("Connection Open...");
+	console.log("Connection Open...");
 });
 
-app.use(cors())
+app.use(cors());
 
 async function getDownloadURI() {
-  console.log('getting uri for download')
-  const res = await  axios.get('https://api.scryfall.com/bulk-data') 
-  const downloadURI = res.data.data[2].download_uri
-  downloadCards(downloadURI);
+	// dropCollection();
+	console.log("getting uri for download");
+	const res = await axios.get("https://api.scryfall.com/bulk-data");
+	const downloadURI = res.data.data[2].download_uri;
+	downloadCards(downloadURI);
+}
 
+async function dropCollection() {
+	await db.dropCollection("cards");
 }
 
 async function sendToDB() {
-  try {
-    let toInsert = [];
-    for (let i = 0; i < newData.length; i++) {
-      toInsert.push(newData[i]);
+	try {
+		let toInsert = [];
+		for (let i = 0; i < newData.length; i++) {
+			toInsert.push(newData[i]);
 
-      const isLastItem = i === newData.length - 1;
-      // every 100 items, insert into the database
-      if (i % 500 === 0 || isLastItem) {
-        await Card.insertMany(toInsert);
-        toInsert = [];
-      }
-    }
-    console.log("Data Inserted");
-   
-  } catch (err) {
-    console.log(err);
-   
-  }
+			const isLastItem = i === newData.length - 1;
+			// every 100 items, insert into the database
+			if (i % 500 === 0 || isLastItem) {
+				await Card.insertMany(toInsert);
+				toInsert = [];
+			}
+		}
+		console.log("Data Inserted");
+	} catch (err) {
+		console.log(err);
+	}
 }
 
-
-
 const downloadCards = async (urlToPrint) => {
-    console.log("Before job instantiation");
-    var file = fs.createWriteStream("allcards.json");
-    https.get(urlToPrint, function (response) {
-      response
-      .on('data', function(data) {
-
-      })
-      .on("end", function(data) {
-      parseCards();
-      })
-        .pipe(file);
-    });
-
+	console.log("Before job instantiation");
+	var file = fs.createWriteStream("allcards.json");
+	https.get(urlToPrint, function (response) {
+		response
+			.on("data", function (data) {})
+			.on("end", function (data) {
+				parseCards();
+			})
+			.pipe(file);
+	});
 };
 
 // getDownloadURI();
 
 const parseCards = async () => {
-  console.log("Parsing Cards");
-  let count = 0;
-  const stream = fs.createReadStream("allcards.json", { encoding: "utf-8" });
-  const parser = JSONStream.parse("*");
-  stream.pipe(parser);
-  parser.on("data", function (obj) {
-    newData.push(obj);
-    count++;
-  });
+	console.log("Parsing Cards");
+	let count = 0;
+	const stream = fs.createReadStream("allcards.json", { encoding: "utf-8" });
+	const parser = JSONStream.parse("*");
+	stream.pipe(parser);
+	parser.on("data", function (obj) {
+		newData.push(obj);
+		count++;
+	});
 
-  parser.on("end", async(val, i) => {
-    console.log("parse complete");
-   
-    console.log(count + "number of entries");
-   
-    sendToDB();
-  });
+	parser.on("end", async (val, i) => {
+		console.log("parse complete");
+
+		console.log(count + "number of entries");
+
+		sendToDB();
+	});
 };
 
 /* ROUTES */
 
 app.get("/", async (req, res) => {
-  let limit = Math.abs(req.query.limit) || 10;
-  let page = (Math.abs(req.query.page) || 1) - 1;
-  const results = await Card.find()
-    .limit(limit)
-    .skip(limit * page);
-  res.send(results);
+	let limit = Math.abs(req.query.limit) || 10;
+	let page = (Math.abs(req.query.page) || 1) - 1;
+	const results = await Card.find({ "legalities.standard": "legal" })
+		.limit(limit)
+		.skip(limit * page);
+	res.send(results);
 });
 
-app.use('/api/cards', cardsRoute)
+app.use("/api/cards", cardsRoute);
+app.use("/users/", authRoutes);
 
-app.get('/api/search', async (req,res) => { console.log(req.query)
-  let dbQuery = await Card.find({"colors":['G'], "colors":['W'], "colors":['G', 'W']}, null, {limit: 10}).sort({name: 'DESC'})
-  // let dbQuery = await Card.find(req.query, null, {limit: 10})
-  res.send(dbQuery);
-})
+app.get("/api/search", async (req, res) => {
+	console.log(req.query);
+	let dbQuery = await Card.find(
+		{ colors: ["G"], colors: ["W"], colors: ["G", "W"] },
+		null,
+		{ limit: 10 }
+	).sort({ name: "DESC" });
+	// let dbQuery = await Card.find(req.query, null, {limit: 10})
+	res.send(dbQuery);
+});
 
-app.listen("3002", () => console.log(`Server listening on port 3000...`));
-
-
-
+app.listen("3002", () => console.log(`Server listening on port 3002...`));
 
 // const job = new CronJob('0 0 */12 * * *', function() {
 // 	const d = new Date();
@@ -158,5 +159,3 @@ app.listen("3002", () => console.log(`Server listening on port 3000...`));
 // Schema.find()
 //   .limit(limit)
 //   .skip(limit * page);
-
-
--- routes/cards.js
+++ routes/cards.js
@@ -1,21 +1,31 @@
-const {Router} = require('express');
-const Card = require('../models/Card')
+const { Router } = require("express");
+const Card = require("../models/Card");
 const router = Router();
 
-router.get('/', async (req, res) => {
-  let limit = Math.abs(req.query.limit) || 10;
-  let page = (Math.abs(req.query.page) || 1) - 1;
-  const results = await Card.find()
-    .limit(limit)
-    .skip(limit * page);
-  res.send(results);
-})
+router.get("/", async (req, res) => {
+	let limit = Math.abs(req.query.limit) || 100;
+	let page = (Math.abs(req.query.page) || 1) - 1;
+	const results = await Card.findAll()
+		.limit(limit)
+		.skip(limit * page);
+	console.log(results);
+	res.send(results);
+
+	// const results = await Card.find();
+	// console.log(results);
+	// res.send(results);
+});
 
 router.get("/:name", async (req, res) => {
-  let found = await Card.find({
-    name: { $regex: new RegExp("^" + req.params.name), $options: "i" },
-  },null, {sort: {name: 1}}).limit(10);
-  res.send(found);
+	let found = await Card.find(
+		{
+			name: { $regex: new RegExp(req.params.name), $options: "i" },
+			booster: true,
+		},
+		null,
+		{ sort: { name: 1 } }
+	).skip(10);
+	res.send(found);
 });
 
 module.exports = router;
